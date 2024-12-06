83e6cb0fc107c3ca08dc617493f4bd6cb6232602
sic <arabiran@prans.com>
2024-12-06 09:43:41 +0800

    change about content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,12 +6,13 @@ const About = () => {
       <div className="align-element grid md:grid-cols-2 items-center gap-16">
         <img src={aboutSvg} className="w-full h-64" />
         <article>
-          <SectionTitle text="code, matcha and calesthenics" />
+          <SectionTitle text="Code, Matcha, and Calisthenics" />
           <p className="text-slate-600 mt-8 leading-loose">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint
-            maiores itaque doloribus asperiores dolorum quas voluptates
-            distinctio, porro beatae quam provident libero animi totam,
-            praesentium voluptatem corrupti dignissimos ipsum accusantium!
+            Whether it's crafting elegant code, savoring a warm cup of matcha,
+            or perfecting calisthenics moves, I find joy in balancing
+            creativity, mindfulness, and physical strength. Each passion fuels
+            the other, shaping a holistic approach to personal and professional
+            growth.
           </p>
         </article>
       </div>
